5330cf497e3347783505529aa7112f63dfac390a
Alaa-eddine K. <alaa.eddine@gmail.com>
2025-06-23 14:26:43 +0200

    Vault keys note

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+//IMPORTANT NOTE : Your API keys are configured in one of the following files :
+//  .smyth/.sre/vault.json
+//  ~/.smyth/.sre/vault.json
+
+//Edit the vault.json file to update your API keys
